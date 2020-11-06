c8880246a256b704a8babfc9fcb02786d9d2d3aa
Bruno Angeles <bruno.angeles@unity3d.com>
2020-11-06 10:48:07 -0500

    Add a project tests class

---
+++ dummyproject/Assets/Tests/ProjectTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace DummyPackage.SampleProject
+{
+    public class ProjectTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [UnityTest]
+        public IEnumerator ProjectTests_TestOne()
+        {
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
